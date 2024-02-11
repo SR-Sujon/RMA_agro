56130102031191229e98e13d5f1d2f18ecbc37af
Saidur Rahman Sujon <sr.sujon.cyb@gmail.com>
2024-02-11 15:12:40 +0600

    updated to version 2

--- main.js
+++ main.js
@@ -8,3 +8,21 @@ function hideSidebar(){
     const sidebar= document.querySelector('.sidebar');
     sidebar.style.display = 'none';
 }
+
+const navLinkEls = document.querySelectorAll('.nav-link');
+const sectionEls = document.querySelectorAll('.section-class');
+let currentSection = 'home';
+window.addEventListener('scroll', () => {
+    sectionEls.forEach(sectionEL => {
+        if (window.scrollY >= sectionEL.offsetTop) {
+            currentSection = sectionEL.id;
+        }
+    });
+
+    navLinkEls.forEach(navLinkEl => {
+        if (navLinkEl.href.includes(currentSection)){
+            document.querySelector('.active').classList.remove('active');
+            navLinkEl.classList.add('active');
+        }
+    });
+});
