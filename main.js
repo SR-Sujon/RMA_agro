4b9b4946e38ce663e15b41692b1fd85a4349b62d
Saidur Rahman Sujon <sr.sujon.cyb@gmail.com>
2024-03-18 18:58:12 +0600

    updated v-18-Mar-24

--- main.js
+++ main.js
@@ -1,3 +1,24 @@
+// Smooth scrolling function
+function smoothScroll(target) {
+    const element = document.querySelector(target);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start' // or 'end' or 'center' depending on your preference
+      });
+    }
+  }
+  
+  // Event listener for smooth scrolling when clicking on navigation links
+  document.querySelectorAll('.nav-link').forEach(link => {
+    link.addEventListener('click', function(event) {
+      event.preventDefault(); // Prevent default link behavior
+      const targetId = this.getAttribute('href'); // Get the href attribute value
+      smoothScroll(targetId); // Call smoothScroll function with the target ID
+    });
+  });
+  
+
 function showSidebar(){
     const sidebar= document.querySelector('.sidebar');
     sidebar.style.display = 'flex';
